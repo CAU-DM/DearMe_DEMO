b86e803adab833b23f9373fb2d6062266fd7bd2f
minseok128 <minseok128128@gmail.com>
2024-05-06 23:14:19 +0900

    Update App.js : user login page 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [messages, setMessages] = useState([
     { text: "안녕? 오늘 하루는 어땠어?", sender: "gpt" },
   ]);
+  const [user, setUser] = useState(null);
+
   const dt_0 =
     "이제 슬슬 꽃도 피고 점점 따뜻해지고 있다. 이게 정말 봄이 오고 있는 느낌이다. 수업 쉬는시간에 팀원이 천혜향을 가져와서 나눠먹고, 밤에는 친한 동기랑 술도 마셨다. 좋은 사람들과 함께해서인지 날씨가 좋아서인지 오늘은 정말 기분이 좋은 날이었다 !";
   const dt_1 = "오늘은 참 재미있는 하루였다!";
@@ -18,9 +20,14 @@ function App() {
 
   return (
     <div className="App">
-      <OAuthTest />
-      <FeedPage feeds={feeds} />
-      <ChatWindow messages={messages} setMessages={setMessages} />
+      {!user ? (
+        <OAuthTest onLogin={setUser} />
+      ) : (
+        <>
+          <FeedPage feeds={feeds} />
+          <ChatWindow messages={messages} setMessages={setMessages} />
+        </>
+      )}
     </div>
   );
 }
