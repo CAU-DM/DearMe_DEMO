f1a9f052c62c1f912d9b04f9a10bcad1087f6b81
minseok128 <minseok128128@gmail.com>
2024-05-06 23:14:50 +0900

    Update OAuthTest.js : 로그인 페이지로 구성, 기존 로그인 정보가 있다면, 이를 사용

--- frontend/src/OAuthTest.js
+++ frontend/src/OAuthTest.js
@@ -1,32 +1,41 @@
 import { auth } from "./firebase-config";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { useState } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
+import styles from "./OAuthTest.module.css";
 
-function OAuthTest() {
-  const [userData, setUserData] = useState(null);
+function OAuthTest({ onLogin }) {
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user ? onLogin(user) : onLogin(null);
+    });
+  }, []);
 
   function handleGoogleLogin() {
-    const provider = new GoogleAuthProvider(); // provider 구글 설정
-    signInWithPopup(auth, provider) // 팝업창 띄워서 로그인
+    setPersistence(auth, browserLocalPersistence) // 로그인 정보를 localStorage에 저장
+      .then(() => {
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider);
+      })
       .then((data) => {
-        setUserData(data.user); // user data 설정
-        console.log(data); // console에 UserCredentialImpl 출력
+        onLogin(data.user); // App 컴포넌트에 사용자 정보 전달
       })
       .catch((err) => {
         console.log(err);
+        onLogin(null); // 오류 발생 시 로그인 정보 없음으로 처리
       });
   }
 
   return (
-    <div>
-      <h3>구글 로그인 테스트</h3>
-      <button onClick={handleGoogleLogin}>로그인</button>
-      <h4>로그인하면 아래쪽에 이름이 나타납니다.</h4>
-      <div>
-        {userData
-          ? "당신의 이름은 : " + userData.displayName
-          : "로그인 하세요."}
-      </div>
+    <div className={styles.container}>
+      <h3 className={styles.title}>DearMe</h3>
+      <button className={styles.loginButton} onClick={handleGoogleLogin}>
+        구글로 로그인
+      </button>
     </div>
   );
 }
