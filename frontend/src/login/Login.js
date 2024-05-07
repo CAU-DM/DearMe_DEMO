b39dbab185886355f2214a4161f23a8a36ab780c
minseok128 <minseok128128@gmail.com>
2024-05-07 13:52:55 +0900

    Update Project : login관련 기능 dir에 정리 및 legacy 삭제

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,53 +1,62 @@
 import { auth } from "./firebase-config";
 import {
   GoogleAuthProvider,
+  GithubAuthProvider,
   signInWithPopup,
   setPersistence,
   browserLocalPersistence,
 } from "firebase/auth";
 import { useState, useEffect } from "react";
+import styles from "./Login.module.css";
 
-function OAuthTest({ onLogin }) {
+function Login({ onLogin }) {
+  const [disabled, setDisabled] = useState(true);
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      user ? onLogin(user) : onLogin(null);
+      if (user) onLogin(user);
+      else {
+        onLogin(null);
+        setDisabled(false);
+      }
     });
   }, []);
 
   function handleGoogleLogin() {
+    setDisabled(true);
     setPersistence(auth, browserLocalPersistence) // 로그인 정보를 localStorage에 저장
       .then(() => {
         const provider = new GoogleAuthProvider();
-        return signInWithPopup(auth, provider);
+        return signInWithPopup(auth, provider)
       })
       .then((data) => {
+        console.log(data)
         onLogin(data.user); // App 컴포넌트에 사용자 정보 전달
       })
       .catch((err) => {
         console.log(err);
         onLogin(null); // 오류 발생 시 로그인 정보 없음으로 처리
+        auth.signOut();
+        setDisabled(false);
       });
   }
 
   return (
-    // <div className={styles.container}>
-    //   <h3 className={styles.title}>DearMe</h3>
-    //   <button className={styles.loginButton} onClick={handleGoogleLogin}>
-    //     구글로 로그인
-    //   </button>
-    // </div>
     <div className="login-window">
-      <content>
+      <div>
         <img src="logo512.png" alt="Profile" />
         <p>DearMe</p>
-        <button onClick={handleGoogleLogin}>
-          구글로 로그인
+        <button
+          type="button"
+          className={styles.login_with_google_btn}
+          onClick={() => handleGoogleLogin(1)}
+          disabled={disabled}
+        >
+          Sign in with Google
         </button>
-      
-        {/* <button onClick={onLogin(true)}>임시 login 버튼</button> */}
-      </content>
+      </div>
     </div>
   );
 }
 
-export default OAuthTest;
+export default Login;
