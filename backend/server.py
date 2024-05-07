fc95a4e056a16d4060f013174729d3af15237b76
michang <minseok128128@gmail.com>
2024-05-07 19:07:26 +0900

    Update server.py : db를 초기화

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,39 @@
 from flask import Flask, request, jsonify, session, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 import copy
 import os
 import ai
+# import db
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 app.secret_key = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Element(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(30), nullable=False)
+    content_id = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    image_path = db.Column(db.String(100), nullable=True)
+    chat = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Element('{self.user_id}', '{self.content_id}', '{self.content}', '{self.image_path}', '{self.chat}')"
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 @app.route('/')
 def index():
     session.pop('chat', None)
     session['chat'] = session.get('chat', copy.deepcopy(conversation_history))
+    tmp = Element(id=1, user_id='test', content_id=1, chat="")
+    db.session.add(tmp)
+    db.session.commit()
+    print(Element.query.all())
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/submit_form', methods=['POST'])
@@ -18,13 +42,9 @@ def submit_form():
 
     response_message = ai.generate_chat(client, request.form['message'], session['chat'])
     session.modified = True
-    print("Chat history:", session['chat'])
-
-    response_data = {
-        'status': 'success',
-        'message': response_message
-    }
-    return jsonify(response_data)
+    # print("Chat history:", session['chat'])
+    return jsonify({ 'status': 'success',
+                    'message': response_message })
 
 if __name__ == '__main__':
     client = ai.create_openai_client()
