91d4b49ea315cf76f69558ead5641b8ee233c51e
minseok128 <minseok128128@gmail.com>
2024-05-07 13:03:50 +0900

    Update App.js : login page <OAuthTest>로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,12 @@ import FeedPage from "./FeedPage";
 import ChatWindow from "./ChatWindow";
 import OAuthTest from "./OAuthTest";
 import Header from "./Header";
-import GoogleLoginButton from "./GoogleLoginButton";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [messages, setMessages] = useState([
     { text: "안녕? 오늘 하루는 어땠어?", sender: "gpt" },
   ]);
-  const [user, setUser] = useState(null);
 
   const dt_0 =
     "이제 슬슬 꽃도 피고 점점 따뜻해지고 있다. 이게 정말 봄이 오고 있는 느낌이다. 수업 쉬는시간에 팀원이 천혜향을 가져와서 나눠먹고, 밤에는 친한 동기랑 술도 마셨다. 좋은 사람들과 함께해서인지 날씨가 좋아서인지 오늘은 정말 기분이 좋은 날이었다 !";
@@ -21,31 +19,16 @@ function App() {
     { image: "/img/cheon.png", text: dt_1 },
   ];
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
       {isLoggedIn ? (
         <div className="main_container">
           <FeedPage feeds={feeds} />
           <ChatWindow messages={messages} setMessages={setMessages} />
         </div>
       ) : (
-        <div className="login-window">
-          <content>
-            <img src="logo512.png" alt="Profile"/>
-            <p>DearMe</p>
-            <GoogleLoginButton></GoogleLoginButton>
-            <button onClick={handleLogin}>임시 login 버튼</button>
-          </content>
-        </div>
+        <OAuthTest onLogin={setIsLoggedIn}/>
       )}
     </div>
   );
