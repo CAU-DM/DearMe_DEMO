11f16d7315b997428132142336b2bf1dd5a8b71d
minseok128 <minseok128128@gmail.com>
2024-05-08 17:31:46 +0900

    Update db.py : feed에 필요한 column만을 return하는 함수 작성

--- backend/db.py
+++ backend/db.py
@@ -3,15 +3,25 @@ from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
+
 class Element(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(30), nullable=False)
     feedId = db.Column(db.Integer, nullable=False)
     feed = db.Column(db.Text, nullable=True)
-    feedTime = db.Column(db.DateTime, nullable=True),
+    feedTime = db.Column(db.DateTime, nullable=True)
     state = db.Column(db.Integer, nullable=True)
     image_path = db.Column(db.String(100), nullable=True)
     content = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f"Element('{self.user_id}', '{self.feedId}', '{self.feed}', '{self.image_path}', '{self.content}')"
+
+    def serialize_feed(self):
+        return {
+            "id": self.id,
+            "feedId": self.feedId,
+            "feed": self.feed,
+            "feedTime": self.feedTime,
+            "image_path": self.image_path,
+        }
