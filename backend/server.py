3eb6646280adfb4609af1dd619cb4b2f17394c23
minseok128 <minseok128128@gmail.com>
2024-05-08 16:11:01 +0900

    Update server.py : db 관련 내용 제외 후, db.py import하여 사용

--- backend/server.py
+++ backend/server.py
@@ -4,37 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 import copy
 import os
 import ai
+import db
 
-# import db
+def create_app():
+    app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+    app.secret_key = os.urandom(24)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.db.init_app(app)
+    with app.app_context():
+        db.db.drop_all()
+        db.db.create_all()
+    return app
 
-app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
-app.secret_key = os.urandom(24)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-
-class Element(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(30), nullable=False)
-    feedId = db.Column(db.Integer, nullable=False)
-    feed = db.Column(db.Text, nullable=True)
-    image_path = db.Column(db.String(100), nullable=True)
-    content = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return f"Element('{self.user_id}', '{self.feedId}', '{self.feed}', '{self.image_path}', '{self.content}')"
-
-
-with app.app_context():
-    db.create_all()
-
+app = create_app()
 
 @app.route("/")
 def index():
     session.pop("chat", None)
     return send_from_directory(app.static_folder, "index.html")
-
 
 @app.route("/login-success", methods=["POST"])
 def login_success():
@@ -43,18 +31,18 @@ def login_success():
     email = user_data["email"]
     displayName = user_data["displayName"]
 
-    nextId = Element.query.count() + 1
+    nextId = db.Element.query.count() + 1
     print(f"UID: {uid}, Email: {email}, DisplayName: {displayName}, nextId: {nextId}")
-    uIdList = Element.query.filter_by(user_id=uid).order_by(Element.id.desc()).first()
+    uIdList = db.Element.query.filter_by(user_id=uid).order_by(db.Element.id.desc()).first()
     if uIdList is None:
-        tmp = Element(
+        tmp = db.Element(
             id=nextId,
             user_id=uid,
             feedId=nextId,
             content=json.dumps(conversation_history, ensure_ascii=False),
         )
-        db.session.add(tmp)
-        db.session.commit()
+        db.db.session.add(tmp)
+        db.db.session.commit()
         tmpElement = tmp
         print("New user added")
     else:
@@ -77,13 +65,13 @@ def submit_form():
     )
     session.modified = True
     tmpElement = (
-        Element.query.filter_by(user_id=session["uid"])
-        .order_by(Element.id.desc())
+        db.Element.query.filter_by(user_id=session["uid"])
+        .order_by(db.Element.id.desc())
         .first()
     )
     tmpElement.content = json.dumps(session["chat"], ensure_ascii=False)
-    db.session.commit()
-    print(Element.query.order_by(Element.id.desc()).first())
+    db.db.session.commit()
+    print(db.Element.query.order_by(db.Element.id.desc()).first())
     return jsonify({"status": "success", "message": response_message})
 
 
@@ -94,17 +82,16 @@ def generate_form():
     response_message = ai.generate_diary(client, session["chat"])
     session.modified = True
     tmpElement = (
-        Element.query.filter_by(user_id=session["uid"])
-        .order_by(Element.id.desc())
+        db.Element.query.filter_by(user_id=session["uid"])
+        .order_by(db.Element.id.desc())
         .first()
     )
     tmpElement.content = json.dumps(session["chat"], ensure_ascii=False)
-    db.session.commit()
+    db.db.session.commit()
     print("Chat history:", session["chat"])
 
     response_data = {"status": "success", "message": response_message}
     return jsonify(response_data)
-
 
 if __name__ == "__main__":
     client = ai.create_openai_client()
