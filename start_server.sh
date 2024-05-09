1813a89c9c9b092ef217e82f18b2489bc01a4852
michang <minseok128128@gmail.com>
2024-05-09 22:33:58 +0900

    Update start_server.sh : 정적웹으로 배포 후, 서버 백그라운드로 실행

--- start_server.sh
+++ start_server.sh
@@ -2,4 +2,6 @@
 
 echo "start_server.sh is running!"
 cd frontend && npm run build && cd ..
-cd backend && source myenv/bin/activate && python3 server.py && cd ..
+cd backend && source myenv/bin/activate
+nohup sqlite_web -H 0.0.0.0 -p 8080 ./instance/site.db --password &
+nohup python3 backend/server.py &
