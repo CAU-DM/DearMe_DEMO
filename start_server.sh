b3d5ad682171cc7440bc207da14cb4f46aa6a6dc
michang <minseok128128@gmail.com>
2024-05-09 23:09:52 +0900

    Update start_server.sh : typo

--- start_server.sh
+++ start_server.sh
@@ -3,5 +3,5 @@
 echo "start_server.sh is running!"
 cd frontend && npm run build && cd ..
 cd backend && source myenv/bin/activate
-nohup sqlite_web -H 0.0.0.0 -p 8080 .backend/instance/site.db --password &
+nohup sqlite_web -H 0.0.0.0 -p 8080 ./backend/instance/site.db --password &
 nohup python3 backend/server.py &
