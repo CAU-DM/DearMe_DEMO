918d85efdf87eedffe39c018163aaf6816754367
michang <minseok128128@gmail.com>
2024-05-19 13:49:23 +0900

    Update db.py : 재설계된 db 스키마 적용

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+from datetime import datetime 
 
 db = SQLAlchemy()
 
@@ -25,3 +26,45 @@ class Element(db.Model):
             "feedTime": self.feedTime,
             "image_path": self.image_path,
         }
+
+from enum import Enum
+
+class SenderEnum(Enum):
+    USER = 0
+    ASSISTANT = 1
+    SYSTEM = 2
+
+class User(db.Model):
+    __tablename__ = 'user'
+    UId = db.Column(db.Integer, primary_key=True)
+    UName = db.Column(db.String, nullable=False)
+    Email = db.Column(db.String, nullable=False)
+    Persona = db.Column(db.Text, nullable=True)
+    
+    Chats = db.relationship('Chat', backref='user', lazy=True)
+
+class Chat(db.Model):
+    __tablename__ = 'chat'
+    ChatId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    UId = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    Date = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    
+    Messages = db.relationship('Message', backref='chat', lazy=True)
+    Diaries = db.relationship('Diary', backref='chat', lazy=True)
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    MessageId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ChatId = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
+    Message = db.Column(db.Text, nullable=False)
+    Sender = db.Column(db.Enum(SenderEnum), nullable=False)
+    Time = db.Column(db.Time, default=datetime.now().time, nullable=False)
+
+class Diary(db.Model):
+    __tablename__ = 'diary'
+    DiaryId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ChatId = db.Column(db.Integer, db.ForeignKey('chat.id'), nullable=False)
+    Content = db.Column(db.Text, nullable=True)
+    ImgURL = db.Column(db.String, nullable=True)
+    CreatedAt = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    UpdatedAt = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
