ec9d45a239af03e13f9830682ac79509a19fcb6a
minseok128 <minseok128128@gmail.com>
2024-05-31 02:40:52 +0900

    Update FeedPage.js : 날짜 클릭하면 해당 피드로 이동

--- frontend/src/feed/FeedPage.js
+++ frontend/src/feed/FeedPage.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react';
-import FeedItem from './FeedItem';
-import styles from './Feed.module.css';
-import { parse } from 'date-fns';
+import React, { useEffect, useRef } from "react";
+import FeedItem from "./FeedItem";
+import styles from "./Feed.module.css";
+import { parse, differenceInDays, isEqual } from "date-fns";
 
 function FeedPage({ userData, feeds, setFeeds, isGenerated, feedDate }) {
   const randomIndex = Math.floor(Math.random() * 4);
   const imgUrl = "/img/empty_" + randomIndex + ".png";
+  const feedRefs = useRef([]);
 
   useEffect(() => {
     const fetchFeeds = async () => {
@@ -21,19 +22,58 @@ function FeedPage({ userData, feeds, setFeeds, isGenerated, feedDate }) {
         console.error("Error fetching feeds:", error);
       }
     };
-    if (userData !== null || isGenerated == true) fetchFeeds();
+    if (userData !== null || isGenerated === true) fetchFeeds();
   }, [userData, isGenerated]);
+
+  useEffect(() => {
+    if (feeds.length > 0 && feedDate) {
+      const parsedFeedDate = parse(feedDate, "yyyy-MM-dd", new Date());
+      let closestFeedIndex = 0;
+      let closestDiff = Infinity;
+
+      feeds.forEach((feed, index) => {
+        const feedDate = parse(
+          feed.created_at,
+          "EEE, dd MMM yyyy HH:mm:ss 'GMT'",
+          new Date()
+        );
+        const diff = differenceInDays(parsedFeedDate, feedDate);
+        if (
+          isEqual(feedDate, parsedFeedDate) ||
+          (diff >= 0 && diff < closestDiff)
+        ) {
+          closestFeedIndex = index;
+          closestDiff = diff;
+        }
+      });
+
+      setTimeout(() => {
+        if (feedRefs.current[closestFeedIndex])
+          feedRefs.current[closestFeedIndex].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+      }, 0);
+    }
+  }, [feeds, feedDate]);
 
   return (
     <div className={styles.feed_page}>
-      <div>{feedDate}</div>
-      {feeds.length === 0 ? <img src={imgUrl} alt="아직 일기가 없어요!" width={300} style={{ marginTop: '200px' }}/> : null}
-      {feeds.map((feed) => (
+      {feeds.length === 0 ? (
+        <img
+          src={imgUrl}
+          alt="아직 일기가 없어요!"
+          width={300}
+          style={{ marginTop: "200px" }}
+        />
+      ) : null}
+      {feeds.map((feed, index) => (
         <FeedItem
           key={feed.id}
           content={feed.content}
           date={feed.created_at}
           image={feed.img_url}
+          ref={(el) => (feedRefs.current[index] = el)}
         />
       ))}
     </div>
