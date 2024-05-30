a5cd91330559f591af291f22ab901a6bb9275e2a
minseok128 <minseok128128@gmail.com>
2024-05-30 21:42:51 +0900

    Update PhotoDrop.js : 이미지 받으면 file useState에 세팅

--- frontend/src/chat/PhotoDrop.js
+++ frontend/src/chat/PhotoDrop.js
@@ -1,29 +1,46 @@
-import React, { useCallback } from 'react';
-import {useDropzone} from 'react-dropzone';
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
-function PhotoDrop() {
-const onDrop = useCallback(acceptedFiles => {
-  
-}, [])
-const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+function PhotoDrop({ file, setFile }) {
+  const onDrop = useCallback((acceptedFiles) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      setFile(reader.result);
+    };
+    reader.readAsDataURL(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".jpeg", ".jpg", ".png"],
+    },
+  });
 
   return (
-  <div {...getRootProps({ style: {
-    padding: '20px',
-    border: '3px dashed #cccccc',
-    borderRadius: '10px',
-    backgroundColor: '#22232c',
-    color: '#f9f9f9',
-    textAlign: 'center',
-    transition: 'border 0.3s ease-in-out'
-  }})}>
-    <input {...getInputProps()} />
-    {
-      isDragActive ?
-      <p>Drop It! 😎</p> :
-      <p>Drag Photo 🌃</p>
-    }
-  </div>
+    <div
+      {...getRootProps({
+        style: {
+          padding: "20px",
+          border: "3px dashed #cccccc",
+          borderRadius: "10px",
+          backgroundColor: "#22232c",
+          color: "#f9f9f9",
+          textAlign: "center",
+          transition: "border 0.3s ease-in-out",
+        },
+      })}
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? <p>Drop It! 😎</p> : <p>Drag Photo 🌃</p>}
+      {file && (
+        <img
+          src={file}
+          alt="Uploaded"
+          style={{ marginTop: "20px", maxWidth: "100%" }}
+        />
+      )}
+    </div>
   );
 }
 
