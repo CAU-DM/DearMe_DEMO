33af63cbf8b2d3f79df68207c01521b2ae961804
michang <minseok128128@gmail.com>
2024-06-21 15:06:15 +0900

    Update moderation.py : 비동기처리를 통해 빠르게 번역, 검사, 생성

--- backend/moderation.py
+++ backend/moderation.py
@@ -1,11 +1,50 @@
 import deepl
 import os
+import ai
+import asyncio
 
-translator = 0
+translator = None
+
+
+def translate_text(text, target_lang):
+    translation = translator.translate_text(text, target_lang=target_lang)
+    return translation.text
+
+
+def create_moderation(text):
+    response = ai.client.moderations.create(input=text)
+    # print("[", text, "]", " <- 에 대한 테스트 수행 결과")
+    # print(*response.results[0].category_scores, sep="\n")
+    return response.results[0].flagged
+
+
+async def moderation(messege_list_for_ai, u1, u2, u3):
+    text = messege_list_for_ai[-1]["content"]
+    loop = asyncio.get_event_loop()
+    gen_content_task = loop.run_in_executor(
+        None, ai.generate_chat, messege_list_for_ai, u1, u2, u3
+    )
+    en_translation_task = loop.run_in_executor(None, translate_text, text, "EN-US")
+    fr_translation_task = loop.run_in_executor(None, translate_text, text, "FR")
+
+    en_text, fr_text, gen_content = await asyncio.gather(
+        en_translation_task, fr_translation_task, gen_content_task
+    )
+
+    # AI 모델 요청을 동시에 수행
+    en_response_task = loop.run_in_executor(None, create_moderation, en_text)
+    fr_response_task = loop.run_in_executor(None, create_moderation, fr_text)
+
+    en_flag, fr_flag = await asyncio.gather(en_response_task, fr_response_task)
+    if en_flag | fr_flag:
+        return gen_content
+    return None
+
 
 def init():
     global translator
-    if translator == 0:
+    if translator is None:
         translator = deepl.Translator(os.getenv("DEEPL_API_KEY"))
+
 
 init()
